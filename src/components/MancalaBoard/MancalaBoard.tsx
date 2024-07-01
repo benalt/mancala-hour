e4587903ee61714acf15db647d568396dae965ab
Ben Alt <ben.r.alt@gmail.com>
2024-06-30 19:56:23 -0500

    some updates

--- src/components/MancalaBoard/MancalaBoard.tsx
+++ src/components/MancalaBoard/MancalaBoard.tsx
@@ -4,6 +4,7 @@ import mancalaBoardClasses from "./MancalaBoard.module.css";
 import Pocket from "./Pocket";
 import Store from "./Store";
 import {
+  MancalaTurnStep,
   applyTurnGameState,
   newMancalaGameState,
   playerCanMakeMove,
@@ -15,6 +16,10 @@ type MancalaBoardParams = {
 
 export default function MancalaBoard({ onGameMessageLog }: MancalaBoardParams) {
   const [gameState, setGameState] = useState(newMancalaGameState());
+  const [pocketsAndStoresState, setPocketsAndStoresState] = useState({
+    pockets: [...gameState.pockets],
+    stores: [...gameState.stores],
+  } as MancalaTurnStep);
 
   function selectPocket(pocketIdx: number): void {
     // player can only select a pocket on thier side
@@ -28,9 +33,25 @@ export default function MancalaBoard({ onGameMessageLog }: MancalaBoardParams) {
       return;
     }
 
-    setGameState((gameStateWas) => {
-      return applyTurnGameState(gameStateWas, pocketIdx);
-    });
+    const newGameState = applyTurnGameState(gameState, pocketIdx);
+    const updates = [...newGameState.lastTurnSteps];
+    setGameState(newGameState);
+
+    const update = updates.shift();
+    if (update) {
+      setPocketsAndStoresState(update);
+    }
+
+    const updateInterval = setInterval(() => {
+      if (updates.length === 0) {
+        clearInterval(updateInterval);
+      } else {
+        const update = updates.shift();
+        if (update) {
+          setPocketsAndStoresState(update);
+        }
+      }
+    }, 200);
   }
 
   let winningPlayerTitle = <></>;
@@ -64,59 +85,59 @@ export default function MancalaBoard({ onGameMessageLog }: MancalaBoardParams) {
     <>
       <h1>Mancala Hour</h1>
       <div
-        className={`${mancalaBoardClasses.mancala_board} ${activePlayerClassname} ${mancalaBoardClasses.player_orientation_1}`}
+        className={`${mancalaBoardClasses.mancala_board} ${activePlayerClassname} ${mancalaBoardClasses.player_orientation_2}`}
       >
         <Pocket
-          stonesCount={gameState.pockets[0]}
+          stonesCount={pocketsAndStoresState.pockets[0]}
           onPocketSelect={() => selectPocket(0)}
         />
         <Pocket
-          stonesCount={gameState.pockets[1]}
+          stonesCount={pocketsAndStoresState.pockets[1]}
           onPocketSelect={() => selectPocket(1)}
         />
         <Pocket
-          stonesCount={gameState.pockets[2]}
+          stonesCount={pocketsAndStoresState.pockets[2]}
           onPocketSelect={() => selectPocket(2)}
         />
         <Pocket
-          stonesCount={gameState.pockets[3]}
+          stonesCount={pocketsAndStoresState.pockets[3]}
           onPocketSelect={() => selectPocket(3)}
         />
         <Pocket
-          stonesCount={gameState.pockets[4]}
+          stonesCount={pocketsAndStoresState.pockets[4]}
           onPocketSelect={() => selectPocket(4)}
         />
         <Pocket
-          stonesCount={gameState.pockets[5]}
+          stonesCount={pocketsAndStoresState.pockets[5]}
           onPocketSelect={() => selectPocket(5)}
         />
-        <Store stonesCount={gameState.stores[0]} />
+        <Store stonesCount={pocketsAndStoresState.stores[0]} />
 
         <Pocket
-          stonesCount={gameState.pockets[6]}
+          stonesCount={pocketsAndStoresState.pockets[6]}
           onPocketSelect={() => selectPocket(6)}
         />
         <Pocket
-          stonesCount={gameState.pockets[7]}
+          stonesCount={pocketsAndStoresState.pockets[7]}
           onPocketSelect={() => selectPocket(7)}
         />
         <Pocket
-          stonesCount={gameState.pockets[8]}
+          stonesCount={pocketsAndStoresState.pockets[8]}
           onPocketSelect={() => selectPocket(8)}
         />
         <Pocket
-          stonesCount={gameState.pockets[9]}
+          stonesCount={pocketsAndStoresState.pockets[9]}
           onPocketSelect={() => selectPocket(9)}
         />
         <Pocket
-          stonesCount={gameState.pockets[10]}
+          stonesCount={pocketsAndStoresState.pockets[10]}
           onPocketSelect={() => selectPocket(10)}
         />
         <Pocket
-          stonesCount={gameState.pockets[11]}
+          stonesCount={pocketsAndStoresState.pockets[11]}
           onPocketSelect={() => selectPocket(11)}
         />
-        <Store stonesCount={gameState.stores[1]} />
+        <Store stonesCount={pocketsAndStoresState.stores[1]} />
       </div>
       {winningPlayerTitle}
       {explaination}
